9e22f2f6a11afbc054de000f59fb0b022f097c93
Erecutoro <alex.li@epitech.eu>
2022-02-02 21:53:30 +0100

    [Add] base for llvm

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,116 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import System.IO
+import System.Environment
+import System.Exit
 
-main :: IO ()
-main = someFunc
+import LLVM.AST
+import qualified LLVM.AST as AST
+import LLVM.AST.Global
+import LLVM.Context
+import LLVM.Module
+
+import Control.Monad.Except
+import Data.ByteString.Char8 as BS
+
+import Parse
+import Data.ByteString.Short
+
+int :: Type
+int = IntegerType 32
+
+toLLVM :: [Definition] -> AST.Module
+toLLVM def = defaultModule
+      { moduleName = "basic"
+      , moduleDefinitions = def
+      }
+
+parseFunc :: String -> Definition
+parseFunc str = GlobalDefinition functionDefaults
+  {
+    name = Name $ toShort $ BS.pack $ fst fName
+  }
+  where
+    Just fName = runParser (parseSome ( parseAnyChar (['a'..'z'] ++ ['A'..'Z']))) str
+
+-- NEED [FUNCTION] HERE instead of String
+genFunc :: [String] -> [Definition]
+genFunc [] = []
+genFunc [x] = [parseFunc x]
+genFunc (x:xs) = parseFunc x : genFunc xs
+
+parser :: String -> IO()
+parser str = withContext $ \context -> 
+    do
+  llvm <- withModuleFromAST context (toLLVM $ genFunc [str]) moduleLLVMAssembly
+  BS.putStrLn llvm
+
+main :: IO()
+main = do
+    args <- getArgs
+    case args of
+      [fileName] -> System.IO.readFile fileName >>= parser
+      _ -> exitWith $ ExitFailure 84
+
+
+
+------------------------------ Example -----------------------------------------
+
+-- defSub :: Definition
+-- defSub = GlobalDefinition functionDefaults
+--   { name = Name 'sub'
+--   , parameters =
+--       ( [ Parameter int (Name 'a') []
+--         , Parameter int (Name 'b') [] ]
+--       , False )
+--   , returnType = int
+--   , basicBlocks = [body]
+--   }
+--   where
+--     body = BasicBlock
+--         (Name 'entry')
+--         [ Name 'result' :=
+--             Sub False  -- no signed wrap
+--                 False  -- no unsigned wrap
+--                 (LocalReference int (Name 'a'))
+--                 (LocalReference int (Name 'b'))
+--                 []]
+--         (Do $ Ret (Just (LocalReference int (Name 'result'))) [])
+
+-- defAdd :: Definition
+-- defAdd = GlobalDefinition functionDefaults
+--   { name = Name 'add'
+--   , parameters =
+--       ( [ Parameter int (Name 'a') []
+--         , Parameter int (Name 'b') [] ]
+--       , False )
+--   , returnType = int
+--   , basicBlocks = [body]
+--   }
+--   where
+--     body = BasicBlock
+--         (Name 'entry')
+--         [ Name 'result' :=
+--             Add False  -- no signed wrap
+--                 False  -- no unsigned wrap
+--                 (LocalReference int (Name 'a'))
+--                 (LocalReference int (Name 'b'))
+--                 []]
+--         (Do $ Ret (Just (LocalReference int (Name 'result'))) [])
+
+-- module_ :: AST.Module
+-- module_ = defaultModule
+--   { moduleName = 'basic'
+--   , moduleDefinitions = [defAdd, defSub]
+--   }
+
+-- toLLVM :: AST.Module -> IO ()
+-- toLLVM mod = withContext $ \contex -> 
+--     do
+--         llvm <- withModuleFromAST contex mod moduleLLVMAssembly
+--         BS.putStrLn llvm
+
+-- main :: IO ()
+-- main = toLLVM module_
