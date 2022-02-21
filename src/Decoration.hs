37375340a6c65640a0d0488f0136db312d17384c
Yichen HUANG <yichen.huang@epitech.eu>
2022-02-21 15:37:58 +0100

    [Add] decorate call

--- src/Decoration.hs
+++ src/Decoration.hs
@@ -8,6 +8,11 @@
 import Data
 import Decoration_AST
 
+decorateList :: [Expr Undetermined] -> Either Error [Expr CONTEXT]
+decorateList [] = Right []
+decorateList (a:as) = decorate a >>= (\ na -> decorateList as >>= Right . (na:))
+
+
 decorate :: Expr Undetermined -> Either Error (Expr CONTEXT)
 decorate (Var name t a)
    | True = Right (Var name t (Ctx ([Varinfo (name, Short)], [Short, Integer, Long])))
@@ -18,4 +23,7 @@ decorate (BinOp op a b)
 decorate (Type t a)
    | True = decorate a >>= Right . Type t
    | otherwise  = Left "Type error"
+decorate (Call name a)
+   | True = decorateList a >>=  Right . Call name
+   | otherwise  = Left "Call error"
 --   | True = Right (BinOp op (decorate a) (decorate b))
