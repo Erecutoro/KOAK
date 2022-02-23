3a8aec3d17e49ca51764483e3692c07ca3e44997
Yichen HUANG <yichen.huang@epitech.eu>
2022-02-23 15:19:29 +0100

    [Add] add State constructor in Expr, add Val in Var, add data Compare and data Statement

--- src/Data.hs
+++ src/Data.hs
@@ -1,22 +1,32 @@
 module Data where
 
 type Name = String
-
-data Op =
-    Add |
-    Sub |
-    Mul |
-    Div
-    deriving (Show)
+type Val = String
 
 data Undetermined =
     Empty
     deriving (Show)
 
-data Deco =
-    None |
-    DecoType String |
-    Set [String]
+data Op =
+    Add |
+    Sub |
+    Mul |
+    Div |
+    Eq
+    deriving (Show)
+
+data Compare =
+    Sup |
+    Inf |
+    Equal |
+    SupEq |
+    InfEq
+    deriving (Show)
+
+data Statement =
+    For |
+    While |
+    If
     deriving (Show)
 
 data Type =
@@ -28,13 +38,9 @@ data Type =
 
 data Expr a =
     BinOp (Expr a) Op (Expr a) |
-    Var Name Type a |
+    Var Name Val Type a |
     Call Name [Expr a] |
-    Func Name [Expr a] Type |
-    Extern Name [Expr a]
-    deriving (Show)
-
-data Par =
-    Fun String |
-    Dec String
+    Func Name [Expr a] [Expr a] Type |
+    Extern Name [Expr a] |
+    State Statement (Expr a) Compare (Expr a) [Expr a]
     deriving (Show)
