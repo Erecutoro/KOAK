9d84af2be5978d55e89e301d1f286135692049fb
Jamal Kansao <jamal.kansao@epitech.eu>
2022-02-25 15:53:53 +0100

    [Add] adding main with file read

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Lib
+import System.Exit
+import System.IO
+import System.Environment
+import Parse
+import Decoration
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    files <- mapM readFile args
+    let commands = callRidwhite files
+    print files
+    print commands
+
+callRidwhite :: [String] -> [String]
+callRidWhite _ = []
+callRidwhite (a:as) = ridLine (ridWhitespace a) : callRidWhite as
+
+ridWhitespace :: String -> String
+ridWhitespace [] = []
+ridWhitespace file = filter (\xs -> (xs /=' ')) file
+
+ridLine :: String -> String
+ridLine [] = []
+ridLine file = filter (\xs -> (xs /='\n')) file
