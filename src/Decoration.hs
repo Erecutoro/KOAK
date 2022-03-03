913bd06cc4013d7f5e58a983bd0d613557972609
Guerlain BLANCHARD <guerlain.blanchard@epitech.eu>
2022-03-03 12:19:08 +0100

    [Edit] improved error messages

--- src/Decoration.hs
+++ src/Decoration.hs
@@ -30,7 +30,7 @@ setArgumentST ((Var name _ _ (VarCtx a)):as) (SymTab st) = setArgumentST as (Sym
 setArgumentST _ st = Right st 
 
 findSymbol :: String -> SymbolTable -> Either Error SymbolInfo  
-findSymbol _ (SymTab []) = Left "No symbol found"
+findSymbol name (SymTab []) = Left ("\ESC[31mERROR\ESC[0m - Variable \"" ++ name ++ "\" has not been defined")
 findSymbol name (SymTab (Varinfo (a, sinfo):as))
     | a == name = Right sinfo
     | otherwise = findSymbol name (SymTab as)
@@ -55,7 +55,7 @@ binOpTypage Decoration_AST.Integer Decoration_AST.Integer = Right Decoration_AST
 binOpTypage Decoration_AST.Integer Decoration_AST.Double  = Right Decoration_AST.Double  
 binOpTypage Decoration_AST.Double Decoration_AST.Integer = Right Decoration_AST.Double 
 binOpTypage Decoration_AST.Double Decoration_AST.Double  = Right Decoration_AST.Double 
-binOpTypage _ _ = Left "Type Error" 
+binOpTypage _ _ = Left "\ESC[31mERROR\ESC[0m - Operation cannot be handled" 
 
 setBinOpTypage :: Expr Ctx -> Expr Ctx -> Either Error EXT_TYPE 
 setBinOpTypage a b = getType a >>= \na -> getType b >>= \nb -> binOpTypage na nb
@@ -63,34 +63,35 @@ setBinOpTypage a b = getType a >>= \na -> getType b >>= \nb -> binOpTypage na nb
 decorateVar :: Expr Undetermined -> SymbolTable  -> Either Error (Expr Ctx)
 decorateVar (Var "none" v t _) st = Right (Var "none" v t (VarCtx Decoration_AST.Double))
 decorateVar (Var name v t _) st = case findSymbol name st of 
-    Right FuncInfo {} -> Left "Var Error - function is not a type"
+    Right FuncInfo {} -> Left "\ESC[31mERROR\ESC[0m - Function has been called as a variable"
     Right BinOpInfo {} -> Right (Var name v t (VarCtx Decoration_AST.Double))
     Left a -> Left a
-decorateVar _ _ = Left "Var Error"
+decorateVar _ _ = Left "\ESC[31mERROR\ESC[0m - An unexpected Error has occured in a variable"
 
 decorateAssignVar :: Expr Undetermined -> SymbolTable -> Either Error (Expr Ctx)
 decorateAssignVar (Var name "none" t _) st = Right (Var name "none" t (VarCtx Decoration_AST.Double))
-decorateAssignVar _ _ = Left "Variable Assignation Error"
+decorateAssignVar Var {} _ = Left "\ESC[31mERROR\ESC[0m - Variable is not valid for an assignation"
+decorateAssignVar _ _ = Left "\ESC[31mERROR\ESC[0m - Assignation can only be done on a variable"
 
 decorateBinOp :: Expr Undetermined -> SymbolTable  -> Either Error (Expr Ctx)
 decorateBinOp (BinOp _ a Eq b _) st = decorateAssignVar a st >>= \na -> decorate b st >>= \nb -> Right (BinOp Data.Double na Eq nb (BinOpCtx Decoration_AST.Double Decoration_AST.Double Decoration_AST.Double))
 decorateBinOp (BinOp _ a op b _) st = decorate a st >>= \na -> decorate b st >>= \nb -> setBinOpTypage na nb >>= \nt -> Right (BinOp Data.Double na op nb (BinOpCtx nt Decoration_AST.Double Decoration_AST.Double))
-decorateBinOp _ _ = Left "Binop Error"
+decorateBinOp _ _ = Left "\ESC[31mERROR\ESC[0m - An unexpected error has occured in a Binop"
 
 decorateCall :: Expr Undetermined -> SymbolTable -> Either Error (Expr Ctx)
 decorateCall (Call name a _) st = case findSymbol name st of
-    Right BinOpInfo {} -> Left "Call Error - type is not a function"
+    Right BinOpInfo {} -> Left "\ESC[31mERROR\ESC[0m - Variable has been called as a function"
     Right (FuncInfo t args_t) -> decorateList a st >>= \na -> Right (Call name na (CallCtx Decoration_AST.Double))
-    Left a -> Left a
-decorateCall _ _ = Left "Call Error"
+    Left a -> Left ("\ESC[31mERROR\ESC[0m - Function \"" ++ name ++ "\" has not been defined")
+decorateCall _ _ = Left "\ESC[31mERROR\ESC[0m - An unknown error has been detected in a function Call"
 
 decorateFunc :: Expr Undetermined -> SymbolTable -> Either Error (Expr Ctx)
 decorateFunc (Func name a t b _) st = decorateArgumentList a st >>= \na -> setArgumentST na st >>= \nst -> decorate b nst >>= \nb -> Right (Func name na t nb FuncCtx)
-decorateFunc _ _ = Left "Func Error"
+decorateFunc _ _ = Left "\ESC[31mERROR\ESC[0m - An unexpected error has occured in a function"
 
 decorateState :: Expr Undetermined -> SymbolTable -> Either Error (Expr Ctx)
 decorateState (State s a cmp b c _) st = decorate a st >>= \na -> decorate b st >>= \nb -> decorate c st >>= \nc -> Right (State s na cmp nb nc StateCtx)
-decorateState _ _ = Left "State Error"
+decorateState _ _ = Left "\ESC[32m[CONGRATULATION!]\ESC[0m - You coded so badly you created an impossible error, go touch some grass now"
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
@@ -112,7 +113,7 @@ getArgument :: [Expr Ctx] -> Either Error [EXT_TYPE ]
 getArgument ((Var _ _ t _):as) = case getArgument as of
     Left a -> Left a
     Right b -> Right (Decoration_AST.Double :b)
-getArgument (_:as) = Left "Argument Error"
+getArgument (_:as) = Left "\ESC[31mERROR\ESC[0m - Argument is not Valid"
 getArgument [] = Right []
 
 setSymbolTable :: Expr Ctx -> SymbolTable -> Either Error SymbolTable 
