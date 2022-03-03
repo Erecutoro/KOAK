29e1cb5c2488178567f38d03b0d6e4842c1270d5
Guerlain BLANCHARD <guerlain.blanchard@epitech.eu>
2022-03-03 15:48:43 +0100

    [Edit] continuation of the decoration ut

--- test/Decoration_UT.hs
+++ test/Decoration_UT.hs
@@ -12,4 +12,5 @@ import Decoration
 import Test.HUnit
 
 decorationTests :: Test
-decorationTests = test [assertEqual "Basic Variable function" "a" "a"]
+decorationTests = test [assertEqual "Basic Variable Test" (Right [Var "none" "2" Custom (VarCtx Decoration_AST.Double)]) (startDecoration (callParser ["2;"]) (SymTab [])),
+                        assertEqual "Basic BinOp Test" (Right [BinOp Data.Double (Var "none" "2" Custom (VarCtx Decoration_AST.Double)) Add (Var "none" "2" Custom (VarCtx Decoration_AST.Double)) (BinOpCtx Decoration_AST.Double Decoration_AST.Double Decoration_AST.Double)]) (startDecoration (callParser ["2 + 2;"]) (SymTab []))]
